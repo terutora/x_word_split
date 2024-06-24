9d1a79d6680e26e9c3386e90e29527d42e2a18f4
terutora <tachi877teru@gmail.com>
2024-06-25 00:24:59 +0900

    commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function splitText() {
+    const inputText = document.getElementById('inputText').value;
+    const maxChars = 140;
+    let chunks = [];
+
+    for (let i = 0; i < inputText.length; i += maxChars) {
+        chunks.push(inputText.slice(i, i + maxChars));
+    }
+
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+    chunks.forEach((chunk, index) => {
+        const chunkDiv = document.createElement('div');
+        const chunkText = document.createElement('span');
+        chunkText.innerText = chunk;
+
+        const copyButton = document.createElement('button');
+        copyButton.innerText = 'コピー';
+        copyButton.className = 'copy-button';
+        copyButton.onclick = () => copyToClipboard(chunk);
+
+        chunkDiv.appendChild(chunkText);
+        chunkDiv.appendChild(copyButton);
+        resultDiv.appendChild(chunkDiv);
+    });
+}
+
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        alert('コピーしました: ' + text);
+    }, () => {
+        alert('コピーに失敗しました');
+    });
+}
